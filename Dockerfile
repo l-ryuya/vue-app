aafa37e4005dc57d9cda0b77f15e38253924da44
shimoji <shimoji@linus.co.jp>
2025-07-29 16:13:59 +0900

    firebase設定完了

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,8 @@ FROM node@sha256:5539840ce9d013fa13e3b9814c9353024be7ac75aca5db6d039504a56c04ea5
 
 WORKDIR /app
 
+RUN npm install -g firebase-tools
+
 COPY vue-project/package*.json ./
 
 RUN npm install
--- vue-project/vite.config.js
+++ vue-project/vite.config.js
@@ -1,23 +1,26 @@
 import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/vue-app/',
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  // mode に応じた .env.<mode> ファイルを読み込む
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    base: env.VITE_BASE_URL || '/',
+    plugins: [
+      vue(),
+      vueDevTools(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
     },
-  },
-  server: {
-    host: '0.0.0.0',
-    port: 5173
+    server: {
+      host: '0.0.0.0',
+      port: 5173
+    }
   }
-})
+});
